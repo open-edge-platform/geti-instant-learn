0ed7b034b25281014fcdb95867b4ccf9b27d5988
Samet Akcay <samet.akcay@intel.com>
2025-09-26 15:24:44 +0100

    test: Remove mock server handlers from header tests

--- application/ui/src/components/header.test.tsx
+++ application/ui/src/components/header.test.tsx
@@ -5,37 +5,14 @@
 
 import { render } from '@geti-prompt/test-utils';
 import { screen, waitForElementToBeRemoved } from '@testing-library/react';
-import { HttpResponse } from 'msw';
-import { http, server } from 'src/setup-test';
 
 import { Header } from './header.component';
 
 describe('Header', () => {
     it('renders header properly', async () => {
-        server.use(
-            http.get('/api/v1/projects', () => {
-                return HttpResponse.json({
-                    projects: [
-                        {
-                            id: '1',
-                            name: 'Project #1',
-                        },
-                    ],
-                });
-            }),
-
-            http.get('/api/v1/projects/{project_id}', () => {
-                return HttpResponse.json({
-                    id: '1',
-                    name: 'Project #1',
-                });
-            })
-        );
-
         render(<Header />);
 
         await waitForElementToBeRemoved(screen.getByRole('progressbar'));
-        // screen.debug();
         expect(await screen.findByText('Geti Prompt')).toBeInTheDocument();
     });
 });
