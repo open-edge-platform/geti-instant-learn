4690ed4dd9e7d571ec80d9dcea571f3d7a9170cc
Dawid Wesołowski <dawid.wesolowski@intel.com>
2025-10-20 16:31:57 +0200

    fix: Invalidate the cache after project update

--- application/ui/src/features/projects-management/hooks/use-update-project.hook.ts
+++ application/ui/src/features/projects-management/hooks/use-update-project.hook.ts
@@ -4,8 +4,10 @@
  */
 
 import { $api, type ProjectUpdateType } from '@geti-prompt/api';
+import { useQueryClient } from '@tanstack/react-query';
 
 export const useUpdateProject = () => {
+    const queryClient = useQueryClient();
     const updateProjectMutation = $api.useMutation('put', '/api/v1/projects/{project_id}', {
         meta: {
             invalidates: [['get', '/api/v1/projects']],
@@ -13,14 +15,33 @@ export const useUpdateProject = () => {
     });
 
     const updateProjectName = (id: string, body: ProjectUpdateType): void => {
-        updateProjectMutation.mutate({
-            body,
-            params: {
-                path: {
-                    project_id: id,
+        updateProjectMutation.mutate(
+            {
+                body,
+                params: {
+                    path: {
+                        project_id: id,
+                    },
                 },
             },
-        });
+            {
+                onSuccess: () => {
+                    queryClient.invalidateQueries({
+                        queryKey: [
+                            'get',
+                            '/api/v1/projects/{project_id}',
+                            {
+                                params: {
+                                    path: {
+                                        project_id: id,
+                                    },
+                                },
+                            },
+                        ],
+                    });
+                },
+            }
+        );
     };
 
     return updateProjectName;
