a90494a52b111dc64fdb0f479ce543d67d63ab9f
Dawid Wesołowski <dawid.wesolowski@intel.com>
2025-10-24 13:20:39 +0200

    feat: Display captured frame

---
+++ application/ui/src/features/prompt-sidebar/prompt-modes/visual-prompt/captured-image/captured-frame-layout.component.tsx
@@ -0,0 +1,52 @@
+/**
+ * Copyright (C) 2025 Intel Corporation
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { CSSProperties } from 'react';
+
+import { Grid, minmax, View } from '@geti/ui';
+
+import { useSelectedFrame } from '../../../../stream/selected-frame-provider.component';
+import { CapturedFrameActions } from './captured-frame-actions.component';
+import { CapturedFrame } from './captured-frame/captured-frame.component';
+import { Labels } from './labels-management/labels.component';
+import { ZoomProvider } from './zoom/zoom.provider';
+
+export const CapturedFrameLayout = () => {
+    const { selectedFrameId } = useSelectedFrame();
+
+    const disabledStyles: CSSProperties = selectedFrameId === null ? { opacity: 0.5, pointerEvents: 'none' } : {};
+
+    return (
+        <Grid
+            width={'100%'}
+            areas={['labels', 'image', 'actions']}
+            rows={[minmax('size-500', 'auto'), '1fr', 'size-500']}
+            height={'100%'}
+        >
+            <ZoomProvider>
+                <View
+                    gridArea={'labels'}
+                    backgroundColor={'gray-200'}
+                    paddingX={'size-100'}
+                    paddingY={'size-50'}
+                    UNSAFE_style={disabledStyles}
+                >
+                    <Labels />
+                </View>
+                <View gridArea={'image'} backgroundColor={'gray-50'}>
+                    <CapturedFrame frameId={selectedFrameId} />
+                </View>
+                <View
+                    gridArea={'actions'}
+                    backgroundColor={'gray-200'}
+                    padding={'size-100'}
+                    UNSAFE_style={disabledStyles}
+                >
+                    <CapturedFrameActions />
+                </View>
+            </ZoomProvider>
+        </Grid>
+    );
+};
