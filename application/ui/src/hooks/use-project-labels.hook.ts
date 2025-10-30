24a1a774f233642e2980f96318843150923587a9
Joao Vilaca <joao.vilaca@intel.com>
2025-10-30 08:30:42 +0100

    Add useProjectLabels hook

---
+++ application/ui/src/hooks/use-project-labels.hook.ts
@@ -0,0 +1,21 @@
+/**
+ * Copyright (C) 2025 Intel Corporation
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { $api, LabelType } from '@geti-prompt/api';
+import { useProjectIdentifier } from '@geti-prompt/hooks';
+
+export const useProjectLabels = (): LabelType[] => {
+    const { projectId } = useProjectIdentifier();
+
+    const { data } = $api.useSuspenseQuery('get', '/api/v1/projects/{project_id}/labels', {
+        params: {
+            path: {
+                project_id: projectId,
+            },
+        },
+    });
+
+    return data?.labels || [];
+};
