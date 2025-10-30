872c036ab3209fb146115f8ed8c08068b9d19d2e
Joao Vilaca <joao.vilaca@intel.com>
2025-10-30 14:47:43 +0100

    Add hotkeys file

---
+++ application/ui/src/features/annotator/hotkeys/hotkeys.ts
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C) 2025 Intel Corporation
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { isMac } from '@react-aria/utils';
+
+const CTRL_KEY = 'ctrl';
+const COMMAND_KEY = 'meta';
+export const CTRL_OR_COMMAND_KEY = isMac() ? COMMAND_KEY : CTRL_KEY;
+
+export type HotKeyActions = 'undo' | 'redo' | 'toggleAnnotations' | 'deleteAnnotation' | 'enableSam';
+
+export type Hotkeys = Record<HotKeyActions, string>;
+
+export const HOTKEYS: Hotkeys = {
+    undo: `${CTRL_OR_COMMAND_KEY}+z`,
+    redo: `${CTRL_OR_COMMAND_KEY}+y`,
+    toggleAnnotations: 'a',
+    deleteAnnotation: 'delete',
+    enableSam: 's',
+};
