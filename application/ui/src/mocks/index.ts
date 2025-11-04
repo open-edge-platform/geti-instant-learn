14b89aad65b9dd3fac429a80d8d9870d699c20ee
Joao Vilaca <joao.vilaca@intel.com>
2025-11-04 16:00:15 +0100

    Add mocks

---
+++ application/ui/src/mocks/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Copyright (C) 2025 Intel Corporation
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export { getMockedLabel } from './mock-label';
+export { getMockedAnnotation } from './mock-annotation';
+export { getMockedProject } from './mock-project';
