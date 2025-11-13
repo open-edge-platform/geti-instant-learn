3050f34b8aa5c7aee5cd9d9b61fde21dac599714
Joao Vilaca <joao.vilaca@intel.com>
2025-11-13 14:55:40 +0100

    Since spec files now include mocks, we need to update .env file

--- application/ui/tests/env.d.ts
+++ application/ui/tests/env.d.ts
@@ -3,6 +3,40 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+declare module '*.svg' {
+    export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+}
+
+declare module '*.svg?react' {
+    const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+    export default ReactComponent;
+}
+
+declare module '*.module.css' {
+    const classes: { readonly [key: string]: string };
+    export default classes;
+}
+
+declare module '*.module.scss' {
+    const classes: { readonly [key: string]: string };
+    export default classes;
+}
+
+declare module '*.webp' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.png' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.jpg' {
+    const src: string;
+    export default src;
+}
+
 interface ImportMetaEnv {
     // import.meta.env.PUBLIC_API_URL
     readonly PUBLIC_API_URL: string;
