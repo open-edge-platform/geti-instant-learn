c732226dbd97f9a91912656f17a11e7a512f3ffa
Joao Vilaca <joao.vilaca@intel.com>
2025-11-13 08:46:14 +0100

    Add model selection UIs

--- application/ui/src/features/prompts/model-toolbar/model-toolbar.component.tsx
+++ application/ui/src/features/prompts/model-toolbar/model-toolbar.component.tsx
@@ -3,25 +3,41 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Flex, Text } from '@geti/ui';
+import { Flex, Item, Picker, Text } from '@geti/ui';
 
 import styles from './model-toolbar.module.scss';
 
+const useModels = () => {
+    // TODO: replace with actual data
+
+    const mockDate = new Date().toLocaleString('en-US', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: '2-digit',
+        hour12: true,
+    });
+
+    return [
+        { id: '1', name: 'DINO v2', deployedAt: mockDate },
+        { id: '2', name: 'DINO v3', deployedAt: mockDate },
+        { id: '3', name: 'DINO v4', deployedAt: mockDate },
+    ];
+};
+
 export const ModelToolbar = () => {
+    const models = useModels();
+
     return (
-        <Flex alignItems={'baseline'} justifyContent={'space-between'}>
-            <Text>DINO v2</Text>
-            <Text UNSAFE_className={styles.deployedText}>
-                Deployed:{' '}
-                {new Date().toLocaleString('en-US', {
-                    day: '2-digit',
-                    month: 'long',
-                    year: 'numeric',
-                    hour: 'numeric',
-                    minute: '2-digit',
-                    hour12: true,
-                })}
-            </Text>
+        <Flex alignItems={'end'} gap={'size-100'}>
+            <Picker label={'Model'} defaultSelectedKey={models[0].id} items={models}>
+                {(item) => <Item key={item.id}>{item.name}</Item>}
+            </Picker>
+
+            <Flex UNSAFE_className={styles.deployedText}>
+                <Text>Deployed: {models[0].deployedAt}</Text>
+            </Flex>
         </Flex>
     );
 };
