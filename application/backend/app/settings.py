c8f5f8d3654af6e89e4ed956edde3bc37bff3652
Michał Adamczyk <michal1.adamczyk@intel.com>
2025-11-18 12:14:26 +0100

    use localhost

--- application/backend/app/settings.py
+++ application/backend/app/settings.py
@@ -31,7 +31,7 @@ class Settings(BaseSettings):
     static_files_dir: str | None = Field(default=None, alias="STATIC_FILES_DIR")
 
     # Server
-    host: str = Field(default="0.0.0.0", alias="HOST")  # noqa: S104
+    host: str = Field(default="localhost", alias="HOST")
     port: int = Field(default=9100, alias="PORT")
 
     # Database
