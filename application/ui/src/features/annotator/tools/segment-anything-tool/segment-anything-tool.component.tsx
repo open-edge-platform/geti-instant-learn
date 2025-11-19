455d3cc88635ce474bcf0539d4ba449f7d627bef
Joao Vilaca <joao.vilaca@intel.com>
2025-11-19 09:41:41 +0100

    Delete unnecessary code; Move annotationso out of SAM

--- application/ui/src/features/annotator/tools/segment-anything-tool/segment-anything-tool.component.tsx
+++ application/ui/src/features/annotator/tools/segment-anything-tool/segment-anything-tool.component.tsx
@@ -10,17 +10,14 @@ import { clampPointBetweenImage } from '@geti/smart-tools/utils';
 import { useZoom } from '../../../../components/zoom/zoom.provider';
 import { useVisualPrompt } from '../../../prompts/visual-prompt/visual-prompt-provider.component';
 import { AnnotationShape } from '../../annotations/annotation-shape.component';
-import { Annotation } from '../../annotations/annotation.component';
 import { MaskAnnotations } from '../../annotations/mask-annotations.component';
 import { useAnnotationActions } from '../../providers/annotation-actions-provider.component';
-import { useAnnotationVisibility } from '../../providers/annotation-visibility-provider.component';
 import { useAnnotator } from '../../providers/annotator-provider.component';
 import { type Annotation as AnnotationType, type Shape } from '../../types';
 import { SvgToolCanvas } from '../svg-tool-canvas.component';
 import { getRelativePoint, removeOffLimitPoints } from '../utils';
 import { SAMLoading } from './sam-loading.component';
 import { InteractiveAnnotationPoint } from './segment-anything.interface';
-import { useDecodingMutation } from './use-decoding-query.hook';
 import { useSegmentAnythingModel } from './use-segment-anything.hook';
 import { useSingleStackFn } from './use-single-stack-fn.hook';
 import { useThrottledCallback } from './use-throttle-callback.hook';
@@ -45,13 +42,10 @@ export const SegmentAnythingTool = () => {
 
     const zoom = useZoom();
     const { roi, image } = useAnnotator();
+    const { addAnnotations } = useAnnotationActions();
     const { selectedLabel } = useVisualPrompt();
-    const { annotations } = useAnnotationActions();
-    const { isVisible } = useAnnotationVisibility();
     const { isLoading, decodingQueryFn } = useSegmentAnythingModel();
     const throttledDecodingQueryFn = useSingleStackFn(decodingQueryFn);
-
-    const decodingMutation = useDecodingMutation(decodingQueryFn, selectedLabel ? [selectedLabel] : []);
 
     const ref = useRef<SVGRectElement>(null);
 
@@ -100,10 +94,8 @@ export const SegmentAnythingTool = () => {
 
         const point = clampPoint(getRelativePoint(ref.current, { x: event.clientX, y: event.clientY }, zoom.scale));
 
-        decodingMutation.mutate([{ ...point, positive: true }], {
-            onError: (error) => {
-                console.error('Failed to create annotation:', error);
-            },
+        decodingQueryFn([{ ...point, positive: true }]).then((shapes) => {
+            addAnnotations(shapes, selectedLabel ? [selectedLabel] : []);
         });
     };
 
@@ -138,32 +130,26 @@ export const SegmentAnythingTool = () => {
             }}
         >
             {previewAnnotations.length > 0 && (
-                <MaskAnnotations isEnabled annotations={previewAnnotations} width={image.width} height={image.height} />
+                <MaskAnnotations isEnabled annotations={previewAnnotations} width={image.width} height={image.height}>
+                    {previewAnnotations.map((annotation) => (
+                        <g
+                            key={annotation.id}
+                            aria-label='Segment anything preview'
+                            style={
+                                {
+                                    '--energy-blue-shade': '#0095ca',
+                                } as CSSProperties
+                            }
+                            {...SELECT_ANNOTATION_STYLES}
+                            strokeWidth={'calc(3px / var(--zoom-scale))'}
+                            fill={'transparent'}
+                            className={classes.animateStroke}
+                        >
+                            <AnnotationShape annotation={annotation} />
+                        </g>
+                    ))}
+                </MaskAnnotations>
             )}
-
-            <g aria-label={'annotation list'}>
-                {isVisible &&
-                    annotations.map((annotation) => <Annotation annotation={annotation} key={annotation.id} />)}
-            </g>
-
-            {previewAnnotations.length > 0 &&
-                previewAnnotations.map((annotation) => (
-                    <g
-                        key={annotation.id}
-                        aria-label='Segment anything preview'
-                        style={
-                            {
-                                '--energy-blue-shade': '#0095ca',
-                            } as CSSProperties
-                        }
-                        {...SELECT_ANNOTATION_STYLES}
-                        strokeWidth={'calc(3px / var(--zoom-scale))'}
-                        fill={'transparent'}
-                        className={classes.animateStroke}
-                    >
-                        <AnnotationShape annotation={annotation} />
-                    </g>
-                ))}
         </SvgToolCanvas>
     );
 };
