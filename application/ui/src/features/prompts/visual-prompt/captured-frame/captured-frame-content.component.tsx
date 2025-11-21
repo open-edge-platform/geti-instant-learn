264330b8c678ee52b32f6d07f78a2a764fc03665
Joao Vilaca <joao.vilaca@intel.com>
2025-11-21 16:39:33 +0100

    Move Labels

--- application/ui/src/features/prompts/visual-prompt/captured-frame/captured-frame-content.component.tsx
+++ application/ui/src/features/prompts/visual-prompt/captured-frame/captured-frame-content.component.tsx
@@ -9,15 +9,10 @@ import { ZoomProvider } from '../../../../components/zoom/zoom.provider';
 import { AnnotatorActions } from '../../../annotator/actions/annotator-actions.component';
 import { AnnotatorCanvasSettings } from '../../../annotator/actions/settings/annotator-canvas-settings.component';
 import { AnnotatorCanvas } from '../../../annotator/annotator-canvas/annotator-canvas';
-import { Labels } from '../labels-management/labels.component';
 
 export const CapturedFrameContent = () => {
     return (
         <ZoomProvider>
-            <View gridArea={'labels'} backgroundColor={'gray-200'} paddingX={'size-100'} paddingY={'size-50'}>
-                <Labels />
-            </View>
-
             <View gridArea={'image'} backgroundColor={'gray-50'} overflow={'hidden'}>
                 <AnnotatorCanvasSettings>
                     <AnnotatorCanvas />
--- application/ui/src/features/prompts/visual-prompt/visual-prompt.component.tsx
+++ application/ui/src/features/prompts/visual-prompt/visual-prompt.component.tsx
@@ -3,11 +3,12 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Flex, Grid } from '@geti/ui';
+import { Flex, Grid, View } from '@geti/ui';
 
 import { useSelectedFrame } from '../../../shared/selected-frame-provider.component';
 import { CapturedFramePlaceholder } from './captured-frame/captured-frame-placeholder.component';
 import { CapturedFrame, CapturedFrameProviders } from './captured-frame/captured-frame.component';
+import { Labels } from './labels-management/labels.component';
 import { PromptThumbnailList } from './prompt-thumbnails/prompt-thumbnail-list/prompt-thumbnail-list.component';
 import { SavePrompt } from './save-prompt/save-prompt.component';
 
@@ -16,16 +17,22 @@ export const VisualPrompt = () => {
 
     return (
         <Flex direction={'column'} gap={'size-300'} height={'100%'}>
-            <Grid minHeight={'size-6000'} gap={'size-300'}>
-                {selectedFrameId === null ? (
-                    <CapturedFramePlaceholder />
-                ) : (
-                    <CapturedFrameProviders frameId={selectedFrameId}>
-                        <CapturedFrame frameId={selectedFrameId} />
-                        <SavePrompt />
-                    </CapturedFrameProviders>
-                )}
-            </Grid>
+            {selectedFrameId === null ? (
+                <CapturedFramePlaceholder />
+            ) : (
+                <CapturedFrameProviders frameId={selectedFrameId}>
+                    <View gridArea={'labels'} backgroundColor={'gray-200'} paddingX={'size-100'} paddingY={'size-50'}>
+                        <Labels />
+                    </View>
+
+                    <Grid minHeight={'size-6000'} gap={'size-300'}>
+                        <>
+                            <CapturedFrame frameId={selectedFrameId} />
+                            <SavePrompt />
+                        </>
+                    </Grid>
+                </CapturedFrameProviders>
+            )}
 
             <PromptThumbnailList />
         </Flex>
