4886aed9e22e65bc6943aed622332a9450258335
Dawid Wesołowski <dawid.wesolowski@intel.com>
2025-12-01 11:27:06 +0100

    chore: Renamed source card to pipline entity card

--- application/ui/src/features/sources-sinks/sources/sample-dataset/sample-dataset-card.component.tsx
+++ application/ui/src/features/sources-sinks/sources/sample-dataset/sample-dataset-card.component.tsx
@@ -8,7 +8,7 @@ import { Flex, View } from '@geti/ui';
 import { Datasets } from '@geti/ui/icons';
 
 import SampleDatasetImg from '../../../../assets/coffee-berries-placeholder.webp';
-import { SourceCard } from '../source-card/source-card.component';
+import { PipelineEntityCard } from '../../pipeline-entity-card/pipeline-entity-card.component';
 import { SampleDatasetDescription, SampleDatasetTitle } from './create-sample-dataset.component';
 
 interface SampleDatasetCardProps {
@@ -21,17 +21,17 @@ export const SampleDatasetCard = ({ source, onAction, menuItems }: SampleDataset
     const isActiveSource = source.connected;
 
     return (
-        <SourceCard isActive={isActiveSource} icon={<Datasets width={'32px'} />} title={'Sample dataset'}>
+        <PipelineEntityCard isActive={isActiveSource} icon={<Datasets width={'32px'} />} title={'Sample dataset'}>
             <Flex direction={'column'} gap={'size-200'}>
                 <img src={SampleDatasetImg} alt={'Sample dataset'} style={{ display: 'block', width: '100%' }} />
                 <SampleDatasetTitle />
                 <Flex>
                     <SampleDatasetDescription />
                     <View alignSelf={'end'}>
-                        <SourceCard.Menu isActive={isActiveSource} items={menuItems} onAction={onAction} />
+                        <PipelineEntityCard.Menu isActive={isActiveSource} items={menuItems} onAction={onAction} />
                     </View>
                 </Flex>
             </Flex>
-        </SourceCard>
+        </PipelineEntityCard>
     );
 };
