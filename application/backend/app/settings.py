68765d1362ae05748ace5633c9c50ea6fdbbfeba
Mikhail Pryakhin <mikhail.pryakhin@intel.com>
2025-12-03 20:06:33 +0000

    add non root user

--- application/backend/app/settings.py
+++ application/backend/app/settings.py
@@ -7,7 +7,7 @@ from functools import lru_cache
 from pathlib import Path
 from typing import Literal
 
-from pydantic import Field, Json
+from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
