b87344a748328c6f3eafc33074002e668f0ecf48
Romanov, Evgeny <evgeny.romanov@intel.com>
2025-12-03 16:19:30 +0100

    Fixed stylecheck

--- application/backend/pyinstaller/hook-setenv.py
+++ application/backend/pyinstaller/hook-setenv.py
@@ -8,9 +8,13 @@ system = platform.system()
 print("Setup Hook: Detected operating system:", system)
 
 if system == "Windows":
+    local_app_data = os.getenv("LOCALAPPDATA")
+    if not local_app_data:
+        raise OSError("LOCALAPPDATA environment variable is not set.")
+
     import ctypes
 
-    GetCurrentPackageFamilyName = ctypes.windll.kernel32.GetCurrentPackageFamilyName
+    GetCurrentPackageFamilyName = ctypes.windll.kernel32.GetCurrentPackageFamilyName  # type: ignore[attr-defined]
     GetCurrentPackageFamilyName.argtypes = [ctypes.POINTER(ctypes.c_uint), ctypes.c_wchar_p]
     GetCurrentPackageFamilyName.restype = ctypes.c_long
 
@@ -22,7 +26,6 @@ if system == "Windows":
         package_family_name = package_family_name_buffer.value
         print("Setup Hook: Application runs in a UWP context. Package Family Name:", package_family_name)
 
-        local_app_data = os.getenv("LOCALAPPDATA")
         app_data_folder = os.path.join(local_app_data, "Packages", package_family_name, "LocalState")
 
         print("Setup Hook: Using local state folder:", app_data_folder)
