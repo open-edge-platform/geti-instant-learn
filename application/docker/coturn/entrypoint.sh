1b20c02b977b504686ead30ab3d65460ee890d1b
Mikhail Pryakhin <mikhail.pryakhin@intel.com>
2025-12-04 15:05:56 +0000

    code review improvements

--- application/docker/coturn/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-if [ -n "$EXTERNAL_IP" ]; then
-    echo "Configuring external-ip to $EXTERNAL_IP"
-    echo "external-ip=$EXTERNAL_IP" >> /etc/turnserver.conf
-fi
-
-exec "$@"
