9aa46c362a7cc8597b1c68baa1c8a19fa5893568
Dawid Wesołowski <dawid.wesolowski@intel.com>
2025-12-16 13:52:58 +0100

    chore: Use getQueryKey to get fully ts support when invalidating queries

--- application/ui/src/features/prompts/models/api/use-set-active-model.ts
+++ application/ui/src/features/prompts/models/api/use-set-active-model.ts
@@ -5,14 +5,34 @@
 
 import { $api, ModelType } from '@geti-prompt/api';
 import { useProjectIdentifier } from '@geti-prompt/hooks';
+import { useQueryClient } from '@tanstack/react-query';
 
-const useSetActiveModelQuery = () => {
-    return $api.useMutation('put', '/api/v1/projects/{project_id}/models/{model_id}');
+import { getQueryKey } from '../../../../query-client/query-client';
+
+const useSetActiveModelQuery = (projectId: string) => {
+    const queryClient = useQueryClient();
+
+    return $api.useMutation('put', '/api/v1/projects/{project_id}/models/{model_id}', {
+        meta: {
+            invalidates: [
+                ['get', '/api/v1/projects/{project_id}/models', { params: { path: { project_id: projectId } } }],
+            ],
+        },
+        onSuccess: ({ id }) => {
+            queryClient.invalidateQueries({
+                queryKey: getQueryKey([
+                    'get',
+                    '/api/v1/projects/{project_id}/models/{model_id}',
+                    { params: { path: { project_id: projectId, model_id: id } } },
+                ]),
+            });
+        },
+    });
 };
 
 export const useSetActiveModel = () => {
     const { projectId } = useProjectIdentifier();
-    const updateModelMutation = useSetActiveModelQuery();
+    const updateModelMutation = useSetActiveModelQuery(projectId);
 
     return (model: ModelType) => {
         const { id, name, config } = model;
