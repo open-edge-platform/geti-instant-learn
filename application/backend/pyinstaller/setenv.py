de983e770f9d491f91c72270b6fc8c5adf507290
Romanov, Evgeny <evgeny.romanov@intel.com>
2026-01-09 14:26:12 +0100

    Fixed dotenv loading

--- application/backend/pyinstaller/setenv.py
+++ application/backend/pyinstaller/setenv.py
@@ -1,9 +1,14 @@
 # Copyright (C) 2025 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-print("Loading dotenv file")
+import os
+import sys
+
 from dotenv import load_dotenv
-loaded = load_dotenv()
+
+dotenv_path = os.path.join(getattr(sys, "_MEIPASS", ""), ".env")
+print(f"Loading dotenv file {dotenv_path}")
+loaded = load_dotenv(dotenv_path)
 if loaded:
     print("Dotenv loaded")
 else:
