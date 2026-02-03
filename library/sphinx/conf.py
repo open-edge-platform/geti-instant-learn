fade7d13087fc4b50f6318a0ae770ef1a4a7b11a
Eugene Liu <eugene.liu@intel.com>
2026-02-03 15:54:00 +0000

    Refactor: Standardize string quotes in Sphinx configuration

--- library/sphinx/conf.py
+++ library/sphinx/conf.py
@@ -18,10 +18,10 @@ module_path = Path(project_root, "src")
 sys.path.insert(0, str(module_path.resolve()))
 sys.path.insert(0, str(project_root.resolve()))
 
-project = 'Geti Instant Learn'
-copyright = 'Intel Corporation'
-author = 'Intel Corporation'
-release = os.environ.get('VERSION', '1.0')
+project = "Geti Instant Learn"
+copyright = "Intel Corporation"
+author = "Intel Corporation"
+release = os.environ.get("VERSION", "1.0")
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
