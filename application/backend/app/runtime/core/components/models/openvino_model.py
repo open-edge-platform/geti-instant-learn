53c37677f0bd36f61c141b0bee6888dd2749c37e
Mikhail Pryakhin <mikhail.pryakhin@intel.com>
2026-02-13 14:16:10 +0000

    move batch creation to the torch model handler

--- application/backend/app/runtime/core/components/models/openvino_model.py
+++ application/backend/app/runtime/core/components/models/openvino_model.py
@@ -17,16 +17,7 @@ class OpenVINOModelHandler(ModelHandler):
         self._model = model
         self._reference_batch = reference_batch
 
-    def initialise(self) -> None:
-        self._model.fit(self._reference_batch)
-        path = self._model.export(".", Backend.OPENVINO)
-
-        core = openvino.Core()
-        ov_model = core.read_model(str(path))
-
-        self._compiled_model = core.compile_model(ov_model, "GPU")
+    def initialise(self) -> None: ...
 
     def predict(self, inputs: list[InputData]) -> list[dict[str, np.ndarray]]:
-        logger.debug("Inference started: model=%s batch size=%d", type(self._model).__name__, len(inputs))
-        # TODO: Implement proper OpenVINO inference with InputData
-        raise NotImplementedError("OpenVINO inference with InputData is not yet implemented")
+        raise NotImplementedError("OpenVINO inference is not yet implemented")
