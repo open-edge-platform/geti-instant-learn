a8200e1b2de6a5bc7898eaabade8b85fcb2d2450
Mikhail Pryakhin <mikhail.pryakhin@intel.com>
2026-03-02 14:20:35 +0000

    code review fixes

--- application/backend/app/runtime/core/components/sink.py
+++ application/backend/app/runtime/core/components/sink.py
@@ -20,7 +20,7 @@ class Sink(PipelineComponent):
         self._initialized = False
 
     def setup(self, outbound_broadcaster: FrameBroadcaster[OutputData]) -> None:
-        self._out_queue = outbound_broadcaster.register("sink")
+        self._out_queue = outbound_broadcaster.register(self.__class__.__name__)
         self._outbound_broadcaster = outbound_broadcaster
         self._initialized = True
 
@@ -47,4 +47,4 @@ class Sink(PipelineComponent):
             logger.debug("Stopping the sink loop")
 
     def _stop(self) -> None:
-        self._outbound_broadcaster.unregister("sink")
+        self._outbound_broadcaster.unregister(self.__class__.__name__)
