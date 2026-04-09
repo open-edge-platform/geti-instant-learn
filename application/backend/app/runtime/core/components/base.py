7bdb95cf05650f0f17073012d6d1765a49d18f01
Mike Pryakhin <mikhail.pryakhin@intel.com>
2026-04-09 09:19:29 +0100

    Update application/backend/app/runtime/core/components/base.py
    Co-authored-by: Daan Krol <daan.krol@intel.com>

--- application/backend/app/runtime/core/components/base.py
+++ application/backend/app/runtime/core/components/base.py
@@ -154,6 +154,7 @@ class ModelHandler(ABC):
     def predict(self, inputs: list[InputData]) -> list[dict[str, np.ndarray]]:
         pass
 
+    @abstractmethod
     def close(self) -> None:
         """Release underlying resources (e.g. models from GPU memory)."""
         pass
