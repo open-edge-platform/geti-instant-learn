281d0af03fa27626a2d55a615eda8a26ca45d3eb
Rudaya, Elena <elena.rudaya@intel.com>
2026-04-16 10:00:44 +0200

    style fix

--- application/backend/app/runtime/components.py
+++ application/backend/app/runtime/components.py
@@ -7,7 +7,6 @@ from abc import ABC, abstractmethod
 from instantlearn.data.base.batch import Batch
 from sqlalchemy.orm import Session, sessionmaker
 
-from domain.services.project import ProjectService
 from domain.services.schemas.device import AvailableDeviceSchema
 from domain.services.schemas.pipeline import PipelineConfig
 from domain.services.schemas.reader import ReaderConfig
