110d7a7b7378b5167203b0fd8cd537bbd5ad25cb
Rudaya, Elena <elena.rudaya@intel.com>
2026-05-22 16:36:13 +0200

    review upd

--- application/ui/src/features/prompts/models/api/use-get-models.ts
+++ application/ui/src/features/prompts/models/api/use-get-models.ts
@@ -1,23 +1,20 @@
 /**
- * Copyright (C) 2025 Intel Corporation
+ * Copyright (C) 2026 Intel Corporation
  * SPDX-License-Identifier: Apache-2.0
  */
-
-import { $api, ModelListType } from '@/api';
+import { $api } from '@/api';
 import { useProjectIdentifier, usePromptMode } from '@/hooks';
 
 export const useGetModels = () => {
     const { projectId } = useProjectIdentifier();
     const [promptMode] = usePromptMode();
+
     const { data } = $api.useSuspenseQuery('get', '/api/v1/projects/{project_id}/models', {
         params: {
-            path: {
-                project_id: projectId,
-            },
-            query: {
-                prompt_mode: promptMode,
-            },
+            path: { project_id: projectId },
+            query: { prompt_mode: promptMode },
         },
     });
-    return (data as ModelListType).models;
+
+    return data.models;
 };
