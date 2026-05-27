d256a3092d68c9f22a47b5c494218402ad4ad7ae
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-05-27 17:20:17 +0200

    try 3

--- application/ui/src/api/get-base-url.tauri.ts
+++ application/ui/src/api/get-base-url.tauri.ts
@@ -6,7 +6,9 @@
 /* eslint-disable no-underscore-dangle */
 export const getBaseUrl = async () => {
     try {
-        return await window.__TAURI__?.core?.invoke<string>('get_public_api_url');
+        return (
+            (await window.__TAURI__?.core?.invoke<string>('get_public_api_url')) ?? import.meta.env.PUBLIC_API_URL ?? ''
+        );
     } catch {
         return import.meta.env.PUBLIC_API_URL ?? '';
     }
