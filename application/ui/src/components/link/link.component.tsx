83459c26afb0bc79baae6a1d0959903a6ce86668
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-07-13 11:15:19 +0200

    fix: Support external links in tauri context (#1097)

---
+++ application/ui/src/components/link/link.component.tsx
@@ -0,0 +1,12 @@
+/**
+ * Copyright (C) 2026 Intel Corporation
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { ComponentProps } from 'react';
+
+import { Link as GetiLink } from '@geti/ui';
+
+type LinkProps = ComponentProps<typeof GetiLink>;
+
+export const Link = (props: LinkProps) => <GetiLink {...props} />;
