b7346081ca7e136fb6bdc4201ae03105d96b4130
pgrubick <piotr.grubicki@intel.com>
2026-07-16 12:40:44 +0200

    Removed the license check

--- application/backend/app/api/endpoints/projects.py
+++ application/backend/app/api/endpoints/projects.py
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import logging
-import sys
 from typing import Annotated
 from uuid import UUID
 
@@ -63,8 +62,12 @@ def create_project(
     license_service: LicenseServiceDep,
 ) -> Response:
     """Create a new project with the given name."""
-    if sys.platform == "win32" and not license_service.is_accepted():
-        raise LicenseNotAcceptedError("Geti Instant Learn License must be accepted before creating projects.")
+
+    # This check should be called only when backend called from within a Windows application installed with msix.
+    # When backend started just as a python processs, which is the case for install.ps1, this check cannot be called
+    # As this distinction is not straightforward, it will not be called  for now because we do not distribute msix yet.
+    # if sys.platform == "win32" and not license_service.is_accepted():
+    #     raise LicenseNotAcceptedError("Geti Instant Learn License must be accepted before creating projects.")
 
     project = project_service.create_project(payload)
     logger.info(f"Successfully created '{project.name}' project with id {project.id}")
